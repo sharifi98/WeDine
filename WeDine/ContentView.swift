7d7812af5eb63de7724fb8252cc2a6ea7f6a9041
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-12-15 20:00:38 +0100

    Initial Commit

---
+++ WeDine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeDine
+//
+//  Created by Hossein Sharifi on 15/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeDine/WeDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeDineApp.swift
+//  WeDine
+//
+//  Created by Hossein Sharifi on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
