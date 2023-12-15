a5dafc8787a248c85ded6e4ade13dd54b47fb784
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-12-15 20:23:12 +0100

    started on the main view of the application

--- WeDine/ContentView.swift
+++ WeDine/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            ForEach(menu) { section in
+                Section(section.name) {
+                    ForEach(section.items) { item in
+                        Text(item.name)
+                    }
+                }
+            }
         }
-        .padding()
+        .navigationTitle("Menu")
+        .listStyle(.grouped)
     }
 }
 
